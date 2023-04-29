678728ff5336f6bcbea23b2f60df9ec5da0976b7
Jorge <122285477+salasPINE@users.noreply.github.com>
2023-04-29 14:48:26 -0400

    Add files via upload

---
+++ examples/python_header_template.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------
+# Title:   TitleofthisDoc
+# Name:    python_header_template.py
+# Project: Python Crash Course
+# Author:  Jorge
+# Created: 04/29/2023
+#-------------------------------------------------------------------------------
+print "Hello World"
