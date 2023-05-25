76dcc4874c885657f6ff188347ff7d050c51a8d9
salasPINE <122285477+salasPINE@users.noreply.github.com>
2023-05-25 16:22:35 +0000

    replit main import

--- main.py
+++ main.py
@@ -2,16 +2,5 @@
 import sys; print('Python %s on %s' % (sys.version, sys.platform))
 
 # main.py
-import turtle
-t = turtle.Pen()
 
-t.forward(50)
-t.left(90)
-t.forward(50)
-t.left(90)
-t.forward(50)
-t.left(90)
-t.forward(50)
-t.left(90)
-
-turtle.done()
+import turtleCode.ch4pt1
