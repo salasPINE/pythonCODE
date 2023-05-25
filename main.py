70ee0d7eff5341d3321746931c4dabb392021340
Jorge <122285477+salasPINE@users.noreply.github.com>
2023-05-25 09:27:40 -0400

    Turtle VS Code Fixes

--- main.py
+++ main.py
@@ -2,3 +2,16 @@
 import sys; print('Python %s on %s' % (sys.version, sys.platform))
 
 # main.py
+import turtle
+t = turtle.Pen()
+
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+
+turtle.done()
