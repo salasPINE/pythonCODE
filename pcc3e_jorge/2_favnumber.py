f607edccce8533b0bf4e3291a50dda6715310222
Jorge <122285477+salasPINE@users.noreply.github.com>
2024-02-20 13:22:22 -0500

    Ch.2 Try It
    End of Chapter 2 Stuff

---
+++ pcc3e_jorge/2_favnumber.py
@@ -0,0 +1,15 @@
+# FAVORITE NUMBER
+
+favnumber = 13
+
+print(f"My favorite number is {favnumber}")
+
+# THE NUMBER EIGHT
+print("Addition:")
+print(4 + 4)
+print("Subtraction:")
+print(20 - 12)
+print("Multiplication:")
+print(2 * 4)
+print("Division:")
+print(80 / 10)
---
+++ pcc3e_jorge/suffixes_zu.py
@@ -0,0 +1,8 @@
+# Suffixes
+# Prime Zu
+
+filename = "python_notes.txt"
+codefile = "python_code.py"
+
+print(filename.removesuffix(".txt"))
+print(codefile.removesuffix(".py"))
