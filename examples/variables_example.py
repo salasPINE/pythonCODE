c3e63d8e8e3d7d9bac462f05419bf43ccd936630
Jorge <122285477+salasPINE@users.noreply.github.com>
2024-03-18 10:25:21 -0400

    Update variables_example.py

--- examples/variables_example.py
+++ examples/variables_example.py
@@ -1,7 +1,7 @@
-#variables
+# variables
 zero = 0
 eight = 8
-stringvar = 'String Variable'
+stringvar = "String Variable"
 zerostring = "0"
 
 print(zero)
@@ -19,4 +19,4 @@ message = "What did the number %s say to the number %s? nice belt!"
 
 print(f"What did the number {zero} say to the number {eight}? nice belt!")
 
-print(message % (zero,eight))
+print(message % (zero, eight))
