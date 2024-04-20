0442b9868d011d37912884bb1d2433a603ebd170
Jorge Salas <jsalas@Salas-M3.local>
2024-04-19 21:27:42 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,2 @@
 # Run
 print("Hello World")
-
-motorcycles = 'honda', 'yamaha', 'suzuki'
-print(motorcycles)
-
-# pop the item out and assign it to a new variable
-popped_motorcycle = motorcycles.pop()
-print(motorcycles)
-print(popped_motorcycle)
